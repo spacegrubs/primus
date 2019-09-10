6a77bab995e05952af2b9ad50b6a0c9fdb880d0c
Matthew Weidner <matthew.a.weidner@gmail.com>
2019-09-09 20:27:23 -0400

    First Person Controller working, with the ability to shoot enemies. Enemies can take damage, be turned bigger, or move along a transform.

---
+++ Assets/MattScripts/DimensionGun.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionGun : MonoBehaviour
+{
+    [SerializeField] private float rayLength = 10f;
+    [SerializeField] private float damage = 1f;
+    [SerializeField] private GameObject impact;
+
+    private RaycastHit hit;
+    public LayerMask layerMask;
+
+    private GameObject impactRef;
+    private Enemy enemyRef;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip shootingSound;
+    [Range(0.0f, 1.0f)] [SerializeField] float shootingVolume = .4f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.DrawRay(transform.position, transform.forward * rayLength, Color.blue);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        audioSource.PlayOneShot(shootingSound, shootingVolume);
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayLength, layerMask))
+        {
+            impactRef = Instantiate(impact, hit.point, Quaternion.identity);
+            Destroy(impactRef, 1f);
+
+            if (hit.collider.gameObject.tag == "Big")
+            {
+                enemyRef = hit.transform.GetComponent<Enemy>();
+                
+                if(enemyRef != null)
+                {
+                    enemyRef.TakeDamage(damage);
+                }
+            }
+
+            if (hit.collider.gameObject.tag == "Transport")
+            {
+                enemyRef = hit.transform.GetComponent<Enemy>();
+
+                if (enemyRef != null)
+                {
+                    enemyRef.TakeDamage(damage);
+                }
+            }
+
+            print("I hit " + hit.collider.gameObject.name);
+        }
+        else
+        {
+            print("I missed!");
+        }
+    }
+}
---
+++ Assets/MattScripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health;
+
+    public AudioSource audioSource;
+    public AudioClip enemySound;
+    [Range(0.0f, 1.0f)] [SerializeField] float enemyVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        audioSource.PlayOneShot(enemySound, enemyVolume);
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (gameObject.tag == "Big")
+        {
+            gameObject.transform.localScale += new Vector3(1, 1, 1);
+            gameObject.transform.localPosition += new Vector3(0, 1, 0);
+        }
+
+        if(gameObject.tag == "Transport")
+        {
+            gameObject.transform.localPosition += new Vector3(1, 0, 1);
+        }
+    }
+}
