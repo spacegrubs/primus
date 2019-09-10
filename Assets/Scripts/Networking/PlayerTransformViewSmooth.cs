32fc0cb6803fd226a9c7f2a446d604f185972a35
serelium <sam-basket4life@hotmail.com>
2019-09-10 01:09:22 -0400

    Added a smooth serialization/deserialization of player position/rotation

---
+++ Assets/Scripts/Networking/PlayerTransformViewSmooth.cs
@@ -0,0 +1,48 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTransformViewSmooth : MonoBehaviourPun, IPunObservable
+{
+    private Vector3 _truePosition;
+    private Quaternion _trueRotation;
+    private CharacterController _characterController;
+    float _lastUpdateTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+
+        }
+
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, _truePosition, Time.deltaTime * 10) + _characterController.velocity * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, _trueRotation, Time.deltaTime * 10);
+        }
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+
+        else
+        {
+            _truePosition = (Vector3) stream.ReceiveNext();
+            _trueRotation = (Quaternion) stream.ReceiveNext();
+        }
+    }
+}
