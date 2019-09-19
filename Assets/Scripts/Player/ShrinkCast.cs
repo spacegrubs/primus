46bdbc11068a2e359f6087524f9d3b9867822194
eman <tenhitcombo@gmail.com>
2019-09-19 19:33:23 -0400

    Sphere expanding (test code)

--- Assets/Scripts/Player/ShrinkCast.cs
+++ Assets/Scripts/Player/ShrinkCast.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class ShrinkCast : MonoBehaviour
-{
-    private 
+public class ShrinkCast : MonoBehaviour {   
+    [SerializeField] private  GameObject _objSphere;
+    private Vector3 _punchScale = new Vector3(6,6,6);
+    private Transform _sphere;
+    private bool expanding;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DOTween.Init();
+        _sphere = FindObjectOfType<SphereCollider>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X) && expanding == false) {
+            _objSphere.SetActive(true);
+           ExpandSphere();
+        }
+    }
+
+    void ExpandSphere(){
+        expanding = true;
+        //expand the sphere
+        _sphere.DOPunchScale(_punchScale, 1, 1);
+        StartCoroutine("ResetSphereScale");
+    }
+
+    IEnumerator ResetSphereScale(){
+        yield return new WaitForSeconds(1);
+        expanding = false;
+        //_objSphere.SetActive(false);
     }
 }
