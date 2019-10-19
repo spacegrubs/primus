33545b3e311f6e925d75aa6aeaaf070da8d9946c
eman <tenhitcombo@gmail.com>
2019-10-19 18:54:30 -0400

    cleaned out some unecessary code and made it a little more human readable

---
+++ Assets/Eman/Scripts/FSM.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class FSM : MonoBehaviour {
+    // next Destination position of the agent
+    protected Vector3 DestPos;
+    [SerializeField]
+    protected Transform PlayerTransform;
+    protected AICharacterControl AICharacterControl;
+
+    protected virtual void Initialize() {}
+    protected virtual void FSMUpdate() {}
+    protected  virtual void FSMFixedUpdate() {}
+
+    private void Start() {
+        Initialize();
+    }
+
+    private void Update() {
+        FSMUpdate();
+    }
+
+    private void FixedUpdate() {
+        FSMFixedUpdate();
+    }
+}
---
+++ Assets/Eman/Scripts/SimpleFSM.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SimpleFSM : FSM {
+    public enum FSMState {
+        None,
+        Patrol,
+        Chase, 
+        Attack,
+        Dead,
+    }
+
+    // Current state that the NPC is reaching
+    public FSMState currentState;
+
+    protected override void Initialize() {
+        AICharacterControl = GetComponent<AICharacterControl>();
+        currentState = FSMState.Patrol;
+        // Reference to Player object
+        GameObject objPlayer = GameObject.FindGameObjectWithTag("Player");
+        PlayerTransform = objPlayer.transform;
+    }
+
+    protected override void FSMUpdate() {
+        switch (currentState) {
+            case FSMState.Patrol: UpdatePatrolState(); break;
+            case FSMState.Chase: UpdateChaseState(); break;
+        }
+    }
+    /// <summary>
+    /// Patrol State
+    /// </summary>
+    private void UpdatePatrolState() {
+        AICharacterControl.SetTarget(null);
+        if (Vector3.Distance(transform.position, PlayerTransform.position) <= 9.99f) {
+            currentState = FSMState.Chase;
+        }
+    }
+    /// <summary>
+    /// Chase State
+    /// </summary>
+    private void UpdateChaseState() {
+        AICharacterControl.SetTarget(PlayerTransform);
+        float dist = Vector3.Distance(transform.position, PlayerTransform.position);        
+        
+        if (dist >= 10.0f) {
+            currentState = FSMState.Patrol;
+        }
+    }
+}
