b3e8e802c0e005bdab23c7c56b96ef700240701c
eman <tenhitcombo@gmail.com>
2019-10-19 18:47:00 -0400

    Added FSMs for Chase and Patrol

--- Assets/Eman/Scripts/AgentNavigation.cs
+++ Assets/Eman/Scripts/AgentNavigation.cs
@@ -4,86 +4,82 @@ using UnityEngine.AI;
 using System.IO;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class AgentNavigation : MonoBehaviour
-{
+public class AgentNavigation : MonoBehaviour {
     // Inspector assigned variables
-    public AIWaypointNetwork    WaypointNetwork     = null;
-    public int                  CurrentIndex        = 0;
-    public bool                 HasPath             = false;
-    public bool                 PathPending         = false;
-    public bool                 PathStale           = false;
-    public NavMeshPathStatus    PathStatus          = NavMeshPathStatus.PathInvalid;
-    public AnimationCurve       JumpCurve           = new AnimationCurve();
+    public AIWaypointNetwork WaypointNetwork = null;
+    public int CurrentIndex = 0;
+    public bool HasPath = false;
+    public bool PathPending = false;
+    public bool PathStale = false;
+    public NavMeshPathStatus PathStatus = NavMeshPathStatus.PathInvalid;
+    public AnimationCurve JumpCurve = new AnimationCurve();
 
-    // Private memners
+    // Private members
     private NavMeshAgent _navAgent = null;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         _navAgent = GetComponent<NavMeshAgent>();
         //* if waypoint network has no transforms assigned to it, don't error 
-        if (WaypointNetwork == null) return;   
+        if (WaypointNetwork == null) return;
         SetNextDestination(false);
     }
 
-    void SetNextDestination(bool icnrement){
+    public void SetNextDestination(bool increment) {
         if (!WaypointNetwork) return;
 
-        int incStep = icnrement ? 1:0;
+        int incStep = increment ? 1 : 0;
         Transform nextWaypointTransform = null;
         //* set the next waypoint as the one that that follows numerically. Otherwise, don't increment.
         int nextWayPoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count)
-            ? 0 : CurrentIndex + incStep;
+            ? 0
+            : CurrentIndex + incStep;
         nextWaypointTransform = WaypointNetwork.Waypoints[nextWayPoint];
 
         //* as long as the location data for the next destination isn't empty, 
         //* tell the navAgent that its next destination is the next waypoint.
-        if (nextWaypointTransform != null){
+        if (nextWaypointTransform != null) {
             CurrentIndex = nextWayPoint;
             _navAgent.destination = nextWaypointTransform.position;
             return;
         }
     }
+
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         //* keep all of the agents navigation information current
-        HasPath     = _navAgent.hasPath;
+        HasPath = _navAgent.hasPath;
         PathPending = _navAgent.pathPending;
-        PathStale   = _navAgent.isPathStale;
-        PathStatus  = _navAgent.pathStatus;
+        PathStale = _navAgent.isPathStale;
+        PathStatus = _navAgent.pathStatus;
 
         //set up OffMeshLink
         if (_navAgent.isOnOffMeshLink)
-            if (_navAgent.isOnOffMeshLink)
-            {
+            if (_navAgent.isOnOffMeshLink) {
                 StartCoroutine(Jump(1.0f));
                 return;
             }
-        
-        if ((_navAgent.remainingDistance <= _navAgent.stoppingDistance && !PathPending) 
+
+        if ((_navAgent.remainingDistance <= _navAgent.stoppingDistance && !PathPending)
             || PathStatus == NavMeshPathStatus.PathInvalid)
-                SetNextDestination(true);
-            else 
-            if (_navAgent.isPathStale)
-                SetNextDestination(false);
+            SetNextDestination(true);
+        else if (_navAgent.isPathStale)
+            SetNextDestination(false);
 
-        IEnumerator Jump(float duration){
-            OffMeshLinkData data    = _navAgent.currentOffMeshLinkData;
-            Vector3 startPos        = _navAgent.transform.position;
-            Vector3 endPos          = data.endPos + (_navAgent.baseOffset * Vector3.up);
-            float time              = 0.0f;
+        IEnumerator Jump(float duration) {
+            OffMeshLinkData data = _navAgent.currentOffMeshLinkData;
+            Vector3 startPos = _navAgent.transform.position;
+            Vector3 endPos = data.endPos + (_navAgent.baseOffset * Vector3.up);
+            float time = 0.0f;
 
-            while (time < duration){
-                float t = time/duration;
+            while (time < duration) {
+                float t = time / duration;
                 _navAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + (JumpCurve.Evaluate(t) * Vector3.up);
                 time += Time.deltaTime;
                 yield return null;
             }
 
             _navAgent.CompleteOffMeshLink();
-
         }
     }
 }
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -1,21 +1,21 @@
-using System;
+using UnityEngine.AI;
 using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (NavMeshAgent))]
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
-        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
-        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
+        public NavMeshAgent agent { get; private set; } // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; }   // the character we are controlling
+        public Transform target;                                     // target to aim for
 
 
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
-            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            agent = GetComponentInChildren<NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
 
 	        agent.updateRotation = false;
